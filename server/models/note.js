77628222d05bf58e929fea27ba4a8fabd82fc8ab
NDeekshitha7 <115596359+NDeekshitha7@users.noreply.github.com>
2022-11-18 19:12:36 -0500

    Add files via upload

---
+++ server/models/note.js
@@ -0,0 +1,20 @@
+const notes = [
+  {
+    userId: 115,
+    noteId: 125,
+    noteContent: "I love music",
+  },
+  {
+    userId: 215,
+    noteId: 225,
+    noteContent: "I love reading books",
+  },
+  {
+    userId: 315,
+    noteId: 325,
+    noteContent: "I love to explore",
+  },
+];
+
+let getNote = () => notes;
+module.exports = { getNote };
