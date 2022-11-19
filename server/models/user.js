29ecb5a86409a6837cb1bf93590b9003cde15b63
NDeekshitha7 <115596359+NDeekshitha7@users.noreply.github.com>
2022-11-18 19:12:02 -0500

    Create user.js

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    userName: "cathy123",
+    password: "icecream",
+  },
+  {
+    userName: "fredburger",
+    password: "badpassword",
+  },
+  {
+    userName: "bobbyjones",
+    password: "hi",
+  },
+];
+
+let getUsers = () => users;
+module.exports = { getUsers };
