d8a612662583053339397293847ac6555b724223
NDeekshitha7 <115596359+NDeekshitha7@users.noreply.github.com>
2022-12-07 01:11:12 -0500

    my commits

--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,81 @@
-const users = [
-  {
-    userName: "cathy123",
-    password: "icecream",
-  },
-  {
-    userName: "fredburger",
-    password: "badpassword",
-  },
-  {
-    userName: "bobbyjones",
-    password: "hi",
-  },
-];
+const con = require("./db_connect");
 
-let getUsers = () => users;
-module.exports = { getUsers };
+// Table Creation 
+async function createTable() {
+  let sql=`CREATE TABLE IF NOT EXISTS users (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT userpk PRIMARY KEY(user_id)
+  ); `
+  await con.query(sql);
+}
+createTable();
+
+// grabbing all users in database
+async function getAllUsers() {
+  const sql = `SELECT * FROM users;`;
+  let users = await con.query(sql);
+  console.log(users)
+}
+
+// Create  User - Registering
+async function register(user) {
+  let cUser = await getUser(user);
+  if(cUser.length > 0) throw Error("Username already in use");
+
+  const sql = `INSERT INTO users (user_id, user_name,password )
+    VALUES ("${user.user_id}", "${user.user_name}", "${user.password}");
+  `
+  await con.query(sql);
+  return await login(user);
+}
+
+// Read User -- login user
+async function login(user) { // {userName: "sda", password: "gsdhjsga"}
+  let cUser = await getUser(user); //[{userName: "cathy123", password: "icecream"}]
+  
+  if(!cUser[0]) throw Error("Username not found");
+  if(cUser[0].password !== user.password) throw Error("Password incorrect");
+
+  return cUser[0];
+}
+
+// Update User function
+async function editUser(user) {
+  let sql = `UPDATE users 
+    SET user_name = "${user.user_name}"
+    WHERE userID = ${user.userID}
+  `;
+
+  await con.query(sql);
+  let updatedUser = await getUser(user);
+  return updatedUser[0];
+}
+
+// Delete User function
+async function deleteUser(user) {
+  let sql = `DELETE FROM users
+    WHERE userID = ${user.userID}
+  `
+  await con.query(sql);
+}
+
+// Useful Functions
+async function getUser(user) {
+  let sql;
+
+  if(user.userID) {
+    sql = `
+      SELECT * FROM users
+       WHERE userID = ${user.userID}
+    `
+  } else {
+    sql = `
+    SELECT * FROM users 
+      WHERE user_name = "${user.user_name}"
+  `;
+  }
+  return await con.query(sql);  
+}
+module.exports = { getAllUsers, login, register, editUser, deleteUser};
